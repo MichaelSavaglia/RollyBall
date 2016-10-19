4eb44110a05606acf6a78474c300037be7121529
Michael <rhino1181@hotmail.co.uk>
2016-10-19 21:52:23 +0100

    Update main.lua

--- GameFiles/main.lua
+++ GameFiles/main.lua
@@ -1,17 +1,17 @@
-
-
 function love.load()
   require "Source/MainMenu"
-  require "Source/PlayerController"
+  require "Source/MapGeneration"
   require "Source/MainGame"
-  
+  Menu_Load()
+  Map_Load()
 end
 
 
 function love.draw()
-  
+  Map_Draw()
+  Menu_Draw()
 end
 
 function love.update(dt)
-  
+
 end
