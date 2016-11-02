1453f26ee071a211fcf9729fe3b0deb223194782
STUDENT\b034362e <anthonybridgett@live.co.uk>
2016-11-02 12:53:18 +0000

    Reverse Commit

--- GameFiles/Source/EmptyBlock.lua
+++
@@ -1,9 +0,0 @@
-function eBlock_Load()
-  eBlock = {}
-  eBlock.graph = love.graphics.newImage("sprites/pathtile.png")
-  eBlock.empty = true
-  eBlock.index = -1
-  eBlock.posX = nil
-  eBlock.posY = nil
-  return eBlock
-end
--- GameFiles/main.lua
+++ GameFiles/main.lua
@@ -1,38 +1,31 @@
 function love.load()
   require "Source/MainMenu"
-  require "Source/RowManager"
+  require "Source/MapGeneration"
   require "Source/MainGame"
   require "Source/CollisionHandler"
   require "Source/PlayerController"
-  require "Source/Gameover"
-    
+  
 
   gamestate = "startScreen"
   
   Menu_Load()
   MainGame_Load()
-  GenerateRow()
+  Map_Load()
   MainGame_Load()
-  Gameover_Load()
 end
 
 function love.draw()
-  --reset color every frame
-  love.graphics.setColor(255, 255,255)
+love.graphics.setColor(255, 255,255)
   if(gamestate == "startScreen") then
     
     Menu_Draw()
     
   elseif(gamestate == "game") then
     
-<<<<<<< HEAD
-    DrawRow()  
-=======
->>>>>>> 33b2547f17f5932f3952716b74f59c4feb3b4cd5
+    Map_Draw()
     MainGame_Draw()
     
   elseif(gamestate == "death") then
-    Gameover_Draw()
     
   elseif(gamestate == "scores") then
     
@@ -44,21 +37,13 @@ function love.update(dt)
   if(gamestate == "startScreen") then
     
     Menu_Update()
-
     
   elseif(gamestate == "game") then
     
-    UpdateRow()
     MainGame_Update(dt)
-<<<<<<< HEAD
-   
-=======
-    
->>>>>>> 33b2547f17f5932f3952716b74f59c4feb3b4cd5
+    Map_Update(dt)
     
   elseif(gamestate == "death") then
-    
-    Gameover_Update()
     
   elseif(gamestate == "scores") then
     
@@ -71,8 +56,6 @@ function love.mousepressed(x, y, button)
     Menu_mousepressed(x,y,button)
   elseif gamestate == "game" then
     Player_mousepressed(x, y, button)
-  elseif gamestate == "death" then
-    Gameover_mousepressed(x, y, button) 
   end
 end
 
