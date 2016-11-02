b57db865d32708d68512c85ee5af5ef3477057d3
Michael <rhino1181@hotmail.co.uk>
2016-11-02 22:55:56 +0000

    [+] s014638f - Changed input to support mobile devices

--- GameFiles/conf.lua
+++ GameFiles/conf.lua
@@ -3,4 +3,6 @@ function love.conf(c)
   local window = c.screen or c.window -- love 0.9 renamed "screen" to "window"
   window.width = 540
   window.height = 1920/2
+  c.window.resizable = true
+  c.window.highdpi = true
 end
