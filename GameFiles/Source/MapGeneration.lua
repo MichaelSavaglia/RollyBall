ed0a7bec936d954be8f6ad5e4ac4ba9eefe482fd
BRIDGETT Anthony J <b034362e@student.staffs.ac.uk>
2016-11-02 11:08:13 +0000

    [+] b034362e - changes to procedural generation

--- GameFiles/Source/MapGeneration.lua
+++ GameFiles/Source/MapGeneration.lua
@@ -1,28 +1,79 @@
 function Map_Load()   
   require "Source/TileConfig"
   tileImage = love.graphics.newImage("sprites/wallTile.png")
+  
+  increment = 4
+  change = 5
+  currentTile = 2
+  previousTile = 2
+  math.randomseed( os.time() )
+  newTile = math.random(5) - 1
 end
 
 function Map_Update(dt)
-  for i = 0, 11 do
-    rows[i].y = rows[i].y + (100 * dt)
-    if (rows[i].y > 1188) then
-      rows[i].y = 0 - tileHeight   
+  for i = 0, 9 do --iterates rows
+    tile[i].rowY = tile[i].rowY + increment
+    if (tile[i].rowY >= 972) then -- move rows to top
+      tile[i].rowY = -104
+      change = change - 1
+    end
+  
+    if(tile[i].rowY == -104) then -- only alter rows that have been moved to the top
+      --generate new column
+      if (change == 0) then -- change to new column if change is zero
+        change = 4
+        
+        while newTile == currentTile do 
+          newTile = math.random(5) - 1
+        end
+        currentTile = newTile
+        
+        for j = 0, 4 do -- iterate through tiles in this row
+          if(currentTile > previousTile) then 
+            if (j >= previousTile and j <= currentTile) then
+              tile[i][j].blockActive = false
+            else
+              tile[i][j].blockActive = true
+            end
+          elseif(currentTile < previousTile) then
+            if (j >= currentTile and j <= previousTile) then
+              tile[i][j].blockActive = false
+            else
+              tile[i][j].blockActive = true
+            end
+          end
+        end
+        
+        previousTile = newTile
+        
+      else -- keep new rows the same until there is another change ()
+        for j = 0, 4 do -- iterate through tiles in this row
+          if j == currentTile then
+            tile[i][j].blockActive = false
+          else
+            tile[i][j].blockActive = true
+          end
+        end
+      end
     end
   end
-
+  
+  --run collision detection call here
 end
 
 function Map_Draw()
   love.graphics.setColor(255, 255,255)
-  for i = 0, 11 do
-    for j = 0, 4 do
-      if (squares[j].blockActive == true) then
-        love.graphics.draw(tileImage, rows[i].tile[j].x, rows[i].y)
-      else
-        --set empty tiles to white
-        love.graphics.rectangle("fill", squares[j].x, (i-1) * tileHeight, tileWitdth, tileHeight)
+  love.graphics.rectangle("fill", 0, 0, 540, 960)
+  
+  for i = 0, 9 do --iterates rows
+    
+    for j = 0, 4 do --iterates tiles in row
+      
+      if (tile[i][j].blockActive == true) then
+        love.graphics.draw(tileImage, tile[i][j].x, tile[i].rowY)
       end
+      
     end
+    
   end
 end
--- GameFiles/Source/TileConfig.lua
+++ GameFiles/Source/TileConfig.lua
@@ -1,26 +1,14 @@
 tileWitdth = 108
 tileHeight = 108
-  
 
-squares = {
-  [0] = {x = 0 * tileWitdth, y = tileHeight, blockActive = true},
-  [1] = {x = 1 * tileWitdth, y = tileHeight, blockActive = true},
-  [2] = {x = 2 * tileWitdth, y = tileHeight, blockActive = false},
-  [3] = {x = 3 * tileWitdth, y = tileHeight, blockActive = true},
-  [4] = {x = 4 * tileWitdth, y = tileHeight, blockActive = true}
-}
-
-rows = {
-  [0] = {tile = squares, y = 0 * tileHeight},
-  [1] = {tile = squares, y = 1 * tileHeight},
-  [2] = {tile = squares, y = 2 * tileHeight},
-  [3] = {tile = squares, y = 3 * tileHeight},
-  [4] = {tile = squares, y = 4 * tileHeight},
-  [5] = {tile = squares, y = 5 * tileHeight},
-  [6] = {tile = squares, y = 6 * tileHeight},
-  [7] = {tile = squares, y = 7 * tileHeight},
-  [8] = {tile = squares, y = 8 * tileHeight},
-  [9] = {tile = squares, y = 9 * tileHeight},
-  [10] = {tile = squares, y = 10 * tileHeight},
-  [11] = {tile = squares, y = 11 * tileHeight}
-}
+tile = {}
+for i = 0, 9 do
+  tile[i] = {rowY = i * tileHeight}
+  for j = 0, 4 do
+    if (j == 2) then
+      tile[i][j] = {x = j * tileWitdth, blockActive = false}
+    else
+      tile[i][j] = {x = j * tileWitdth, blockActive = true}
+    end
+  end
+end
