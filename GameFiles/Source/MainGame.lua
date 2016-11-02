48c50f0c4bf094b2febe2a0c0c1df728b2035964
Michael <rhino1181@hotmail.co.uk>
2016-11-02 12:27:33 +0000

    [+] s014638f - Added player/tile collision, sorted out gamestates

--- GameFiles/Source/MainGame.lua
+++ GameFiles/Source/MainGame.lua
@@ -1,13 +1,17 @@
 require "Source/PlayerController"
+require "Source/MapGeneration"
 
 function MainGame_Load()
   Player_Load()
+  Map_Load()   
 end
 
 function MainGame_Update(dt)
   Player_Update(dt)
+  Map_Update(dt)
 end
 
 function MainGame_Draw()
+  Map_Draw()
   Player_Draw()
 end
--- GameFiles/Source/TileConfig.lua
+++ GameFiles/Source/TileConfig.lua
@@ -1,14 +1,17 @@
-tileWitdth = 108
-tileHeight = 108
 
-tile = {}
-for i = 0, 9 do
-  tile[i] = {rowY = i * tileHeight}
-  for j = 0, 4 do
-    if (j == 2) then
-      tile[i][j] = {x = j * tileWitdth, blockActive = false}
-    else
-      tile[i][j] = {x = j * tileWitdth, blockActive = true}
+function Tiles_Load()
+  tileWitdth = 108
+  tileHeight = 108
+
+  tile = {}
+  for i = 0, 9 do
+    tile[i] = {rowY = i * tileHeight}
+    for j = 0, 4 do
+      if (j == 2) then
+        tile[i][j] = {x = j * tileWitdth, blockActive = false}
+      else
+        tile[i][j] = {x = j * tileWitdth, blockActive = true}
+      end
     end
   end
 end
